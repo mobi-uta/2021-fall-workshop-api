8aa4acb668af5e7f43216cf8807c9f26ea00b7ba
Long Nguyen <18nguyenl@gmail.com>
2021-10-10 07:58:53 +0000

    Integrate tailwind

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,5 +1,12 @@
 const bodyParser = require("body-parser");
+const path = require("path");
+const express = require("express");
+const exec = require("child_process").exec;
 
-module.exports = function (app) {
+module.exports = async function (app) {
+  app.use("/", express.static(path.resolve("src/public")));
+
+  await exec("npx tailwindcss -i ../styles/styles.css -o ../public/styles/styles.css");
+
   app.use(bodyParser.json());
 }
